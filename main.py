7e0ea8e8d2da771338bd7571084eea8c52e68b6c
HkrFlores <horacio.flores.a@gmail.com>
2022-04-05 16:51:48 -0500

    added mp3 creation

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import cv2
 import pytesseract
-
+from gtts import gTTS
+import os
 #img = cv2.imread('breakingnews.png')
 img = cv2.imread('images/airportSecurity.png')
 text = pytesseract.image_to_string(img)
 print(text)
+language = 'en'
+myobj = gTTS(text=text, lang=language, slow=False)
+
+myobj.save("reddit.mp3")
+
 
