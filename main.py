d83ebab53ac8982b9809fc1b20ea2d5a154303ad
HkrFlores <horacio.flores.a@gmail.com>
2022-04-05 17:45:44 -0500

    added new accent with tld=com.au

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import pytesseract
 from gtts import gTTS
 import os
 #img = cv2.imread('breakingnews.png')
-img = cv2.imread('images/airportSecurity.png')
+filename = os.listdir('images/')
+print(filename)
+img = cv2.imread('images/ohboy.png')
 text = pytesseract.image_to_string(img)
 print(text)
 language = 'en'
-myobj = gTTS(text=text, lang=language, slow=False)
+myobj = gTTS(text=text, lang=language,tld="com.au", slow=False)
 
-myobj.save("reddit.mp3")
+myobj.save("audio/ohboy.mp3")
 
 
