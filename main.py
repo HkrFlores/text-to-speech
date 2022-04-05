346efd88626447c77d4481b09e1c189d5e56c6e2
HkrFlores <horacio.flores.a@gmail.com>
2022-04-05 18:09:51 -0500

    added loop, recognize current files on folder images/ and create mp3 file with file name into audio/

--- main.py
+++ main.py
@@ -3,14 +3,30 @@ import pytesseract
 from gtts import gTTS
 import os
 #img = cv2.imread('breakingnews.png')
-filename = os.listdir('images/')
-print(filename)
-img = cv2.imread('images/ohboy.png')
-text = pytesseract.image_to_string(img)
-print(text)
+filesname = os.listdir('images/')
+print(filesname)
 language = 'en'
-audio = gTTS(text=text, lang=language,tld="com.au", slow=False)
 
-audio.save("audio/ohboy2.mp3")
+
+for file in filesname:
+    filename = file.strip('.png')
+    imagepath = 'images/' + file
+    img = cv2.imread(imagepath)
+    text = pytesseract.image_to_string(img)
+    print(text)
+    audio = gTTS(text=text, lang=language, slow=False)
+    audiopath = 'audio/' + filename + '.mp3'
+    print(audiopath)
+    audio.save(audiopath)
+
+
+print("Task completed")
+# all this works without for loop
+#img = cv2.imread('images/ohboy.png')
+#text = pytesseract.image_to_string(img)
+#print(text)
+#language = 'en'
+#audio = gTTS(text=text, lang=language,tld="com.au", slow=False)
+#audio.save("audio/ohboy2.mp3")
 
 
